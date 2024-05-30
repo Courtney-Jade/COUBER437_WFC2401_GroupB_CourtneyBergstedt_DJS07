730e2116fe51b0ff0e3006b0fd950072a8d358bf
Courtney-Jade <156655989+Courtney-Jade@users.noreply.github.com>
2024-05-30 22:44:02 +0200

    coded getMemeImage function

--- COUBER437_WFC2401_GroupB_CourtneyBergstedt_DJS07/src/components/Meme.js
+++ COUBER437_WFC2401_GroupB_CourtneyBergstedt_DJS07/src/components/Meme.js
@@ -7,3 +7,22 @@ export default function Meme() {
         randomImage: "http://i.imgflip.com/1bij.jpg" 
     })
     const [allMemes, setAllMemes] = React.useState([])
+
+    React.useEffect(async () => {
+        fetch("https://api.imgflip.com/get_memes")
+            .then(res => res.json())
+            .then(data => setAllMemes(data.data.memes))
+        return () => {
+            
+        }
+    }, [])
+
+    function getMemeImage() {
+        const randomNumber = Math.floor(Math.random() * allMemes.length)
+        const url = allMemes[randomNumber].url
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: url
+        }))
+        
+    }
